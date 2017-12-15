849c66f8d9d908c3db91c0ab83063f8c632b2c0a
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2017-12-15 18:07:21 +0500

    install eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  render() {
+  render () {
     return (
       <button className="square">
         {/* TODO */}
